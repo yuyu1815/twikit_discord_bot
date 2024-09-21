a6a41733c7dd7b0c31d5a0d92a6094f2be444083
yuzumican <83267351+yuyu1815@users.noreply.github.com>
2024-09-22 00:11:12 +0900

    仮アップロード

--- src/Bot.py
+++ src/Bot.py
@@ -6,9 +6,6 @@ from discord.ext import tasks
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
-CHANMEL_NAME = "PC4USHOP"
-old_Msg1_id = 0
-old_Msg2_id = 0
 
 # いつもの呪文
 intents = discord.Intents.default()
@@ -19,15 +16,15 @@ twitter_client = twitter_get.Twitter_Client()
 
 old_time = time.time()
 
-check_time = 1
 @duscord_client.event
 async def on_ready():
   print('Login OK')
   await tree.sync()
+  # twitterログイン
   await twitter_client.load_client()
   loop.start()
 
-#discordとメッセ時に送信
+#discordとコンソールに送信
 async def messege_send(interaction,msg):
   print(msg)
   await interaction.response.send_message(msg)
@@ -71,51 +68,88 @@ async def set(interaction: discord.Interaction,user_name:str):
     # 読み込み失敗
     await messege_send(interaction, '設定jsonを読み込むことができませんでした')
     return
-  else:
-    for i in range(len(json_data["setting_channels"])):
-      if json_data["setting_channels"][i] == channel_id and json_data["twitter_user_ids"][i] == twitter_id:
-        # 登録済み処理
-        # リストから削除
-        del json_data["setting_channels"][i]
-        del json_data["twitter_user_ids"][i]
-        # json書き込み
-        json_make.edit_setting_json(guild_id, json_data["cool_down_time"],json_data["setting_channels"], json_data["twitter_user_ids"])
-        await messege_send(interaction,'削除完了')
-        return
-  await messege_send(interaction,'削除するものがありません')
 
-@tasks.loop(seconds=check_time)
+  for i in range(len(json_data["setting_channels"])):
+    if json_data["setting_channels"][i] == channel_id and json_data["twitter_user_ids"][i] == twitter_id:
+      # 登録済み処理
+      # リストから削除
+      del json_data["setting_channels"][i]
+      del json_data["twitter_user_ids"][i]
+      # json書き込み
+      json_make.json_Load_And_Settings(guild_id,"setting_channels",json_data["setting_channels"])
+      json_make.json_Load_And_Settings(guild_id,"twitter_user_ids",json_data["twitter_user_ids"])
+      await messege_send(interaction,'削除完了')
+      return
+
+  await messege_send(interaction,'削除するものがありません')
+# クールダウンの設定
+@tree.command(name='check-time', description='twitterをチェックする間隔(分)を設定(小数点以下は使えません)')
+async def cool_down(interaction: discord.Interaction,minutes:int):
+  # 1分未満は1分に設定
+  if(minutes <= 1):
+    minutes = 1
+  json_make.json_Load_And_Settings(guild_id, "cool_down_time", minutes)
+
+@tree.command(name='check-setting', description='現在の設定しているチャンネルなどを表示')
+async def check_setting(interaction: discord.Interaction):
+  # ギルドid
+  guild_id = interaction.guild_id
+  # ギルドidからチャンネル、クールダウン、チャンネル、ツイッターidを取得
+  json_data = json_make.load_setting_json(guild_id)
+  settin_channels = json_data["setting_channels"]
+  twitter_user_ids = json_data["twitter_user_ids"]
+  cool_down_time = json_data["cool_down_time"]
+  if json_data is None:
+  else:
+  # 送信部分
+
+
+@tasks.loop(seconds=1)
 async def loop():
   global old_time
+  # 現在の秒数を取得
   now_time = time.time()
+  # ギルドidを取得
   guild_ids = json_make.get_guild_id()
+  if(guild_ids is None):
+    return
   for guild_id in guild_ids:
-    json_data = await json_make.load_setting_json(guild_id)
+    # ギルドidからチャンネル、クールダウン、チャンネル、ツイッターidを取得
+    json_data = json_make.load_setting_json(guild_id)
     if json_data is None:
       # 読み込み失敗
       print('読み込み失敗')
-      return
-    if json_data["cool_down_time"] >= now_time - old_time:
+      #コンティニュー
+    #cool_down_time は分単位
+    if int(json_data["cool_down_time"])*60 >= now_time - old_time:
       # 設定時間に達していない
-      return
-
+      #コンティニュー
+    # 時間リセットs
     old_time = time.time()
-    for channel_id in json_data["setting_channels"]:
+    for channel_id,twitter_user_id  in zip(json_data["setting_channels"],json_data["twitter_user_ids"]):
       #送信先チャンネル指定
-      channel = duscord_client.get_channel(int(channel_id))
+      channel = duscord_client.get_channel(channel_id)
+      # Twitteridから名前に変換
+      twitter_name =
       # ツイート取得
-      tweet_id,next_tweet_id = await twitter_client.Twikit_Msg(CHANMEL_NAME)
+      tweet_id,next_tweet_id = await twitter_client.Twikit_Msg(twitter_name)
+      old_Msg_id = json_make.load_Twitter_Msg(channel_id,twitter_user_id)
+      # 未設定の場合は初期化
+      old_Msg_id = [
+          old_Msg_id[0] is None ? 0 : old_Msg_id[0],
+          old_Msg_id[1] is None ? 0 : old_Msg_id[1]
+      ]
       # 同じ場合スキップ
-      if(tweet_id == old_Msg1_id):
-        return
+      if(tweet_id == old_Msg_id[0]):
+        #コンティニュー
       # 削除された時の対策用
-      elif(tweet_id == old_Msg2_id):
-        return
+      elif(tweet_id == old_Msg_id[1]):
+        #コンティニュー
       # メッセージを更新
-      old_Msg1_id = tweet_id
-      old_Msg2_id = next_tweet_id
+      old_Msg_id = [tweet_id,next_tweet_id]
+      json_make.twitter_msg_edit(old_Msg1_id[0],old_Msg1_id[1])
       # メッセージを送信
-      url = f'https://fxtwitter.com/{CHANMEL_NAME}/status/{tweet_id}'
+      url = f'https://fxtwitter.com/{twitter_name}/status/{tweet_id}'
       print(url)
       await channel.send(url, silent=True)
 
--- src/json_make.py
+++ src/json_make.py
@@ -1,5 +1,5 @@
 import json,os
-
+# cookieを編集してTwikitを用に変換し保存
 def Twitter_New_Json_edit():
     try:
         with open('./json/cookie.json', 'r') as file:
@@ -17,8 +17,20 @@ def Twitter_New_Json_edit():
 
     with open('./json/cookie_edit.json', 'w') as file:
         json.dump(result, file, indent=4)
-import json
 
+# ギルドidから設定チャンネルを取得
+def load_setting_json(guild_id):
+    try:
+        with open('./json/DiscordSetting.json', 'r') as file:
+            data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return None
+    if str(guild_id) in data:
+        return data[str(guild_id)]
+    else:
+        return None
+
+# 以下の設定を保存 ギルドid,ギルドごとのチェック用クールダウン,チャンネル(配列),ツイッターid(配列)
 def edit_setting_json(guild_id, cool_down_time, setting_channels, twitter_user_ids):
     try:
         # guildごとのjsonファイルの読み込み
@@ -39,17 +51,15 @@ def edit_setting_json(guild_id, cool_down_time, setting_channels, twitter_user_i
     with open('./json/DiscordSetting.json', 'w') as file:
         json.dump(data, file, indent=4)
 
-def load_setting_json(guild_id):
-    try:
-        with open('./json/DiscordSetting.json', 'r') as file:
-            data = json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        return None
-    if str(guild_id) in data:
-        return data[str(guild_id)]
-    else:
-        return None
+def json_Load_And_Settings(guild_id,flag_setting_name,flag_string):
+    cool_down_time, setting_channels, twitter_user_ids = load_setting_json(guild_id)
+    edit_setting_json(guild_id,
+                      flag_setting_name == "cool_down_time" ? flag_string : cool_down_time,
+                      flag_setting_name == "setting_channels" ? flag_string : setting_channels,
+                      flag_setting_name == "twitter_user_ids" ? flag_string : twitter_user_ids
+    )
 
+# ギルドid取得
 def get_guild_id():
     try:
         with open('./json/DiscordSetting.json', 'r') as file:
@@ -57,3 +67,24 @@ def get_guild_id():
     except (FileNotFoundError, json.JSONDecodeError):
         return None
     return list(data.keys())
+# twiiterのメッセージを保存
+def twitter_msg_edit( channel_Id, twitter_Id , msg1 , msg2):
+    try:
+        with open('./json/Twitter_msg.json', 'r') as file:
+            data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        data = []
+    data[str(channel_Id)][str(twitter_Id)][0] = msg1
+    data[str(channel_Id)][str(twitter_Id)][1] = msg2
+
+    # JSONファイルに保存
+    with open('./json/Twitter_msg.json', 'w') as file:
+        json.dump(data, file, indent=4)
+# twitterのメッセージの取得
+def load_Twitter_Msg(channel_id,twitter_Id):
+    try:
+        with open('./json/Twitter_msg.json', 'r') as file:
+            data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return None,None
+    return data[str(channel_id)][str(twitter_Id)]
